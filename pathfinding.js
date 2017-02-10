8f582b65e06480425cb81295f4d1eba5e18b8a90
Edmund Korley <korley.em@gmail.com>
2017-02-10 10:55:00 -0500

    Init a 2D grid on Canvas

--- pathfinding.js
+++ pathfinding.js
@@ -1,7 +1,44 @@
+var cols, rows;
+var side = 30;
+var grid = [];
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1200, 1200);
+  cols = floor(width/side);
+  rows = floor(height/side);
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      var cell = new Cell(col, row);
+      grid.push(cell);
+    }
+  }
 }
 
 function draw() {
   background(253, 213, 0);
+
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+}
+
+function Cell(i, j) {
+  this.i = i;
+  this.j = j;
+  this.walls = { left: true, right: true, top: true, bottom: true };
+
+  this.show = function() {
+    var x = this.i * side;
+    var y = this.j * side;
+    stroke(0);
+    if (this.walls.top)
+      line(x, y, x+side, y);
+    if (this.walls.right)
+      line(x+side, y, x+side, y+side);
+    if (this.walls.bottom)
+      line(x+side, y+side, x, y+side);
+    if (this.walls.left)
+      line(x, y+side, x, y);
+  }
 }
